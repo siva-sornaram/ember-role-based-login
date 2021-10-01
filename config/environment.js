3e71273744a531c5bd1b1b34a36da2c81086025c
Siva Sornaram <mailmeatvignesh7@gmail.com>
2021-10-01 14:21:38 +0530

    zoogle drive

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'ember-role-based-login',
     environment,
-    rootURL: '/role-based-login',
+    rootURL: '/drive.zoogle.com',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
